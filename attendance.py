49821c225833a98b2f89dd9671adb0be7e309815
Mr. Enrico <denrico@northbergen.k12.nj.us>
2016-04-21 11:47:53 -0400

    Attendance tracking working

--- attendance.py
+++ attendance.py
@@ -1,13 +1,18 @@
 __author__ = 'student'
 
 import time
+import shelve
+
+s = shelve.open('students', writeback=True)
 
 while True:
-    try:
-        s_id = input('Scan student id or enter Q to quit: ')
-        if s_id in ['q', 'Q']:
-            break
-        else:
-    	    print('%s checked in - %s' % (student_info[s_id], time.strftime("%I:%M %p")))
-    except KeyError:
-        print("Invalid ID number")
+	try:
+		s_id = input('Scan student id or enter Q to quit: ')
+		if s_id in ['q', 'Q']:
+			s.close()
+			break
+		else:
+			s[s_id]['present'].append(time.strftime("%m/%d at %I/%M"))
+			print('%s checked in on %s' % (s[s_id]['name'], time.strftime("%m/%d at %I:%M")))
+	except KeyError:
+		print("Invalid ID number")
