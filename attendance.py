b1f975bd35a0c8b76a6304810b1a480f32c35b07
Dave Enrico <denrico@northbergen.k12.nj.us>
2016-04-21 11:10:30 -0400

    Remove student dictionary from attendance.py

--- attendance.py
+++ attendance.py
@@ -2,20 +2,6 @@ __author__ = 'student'
 
 import time
 
-student_info = {'4401001': 'Christine Adewale',
-'44001722': 'Wayne Bertrand',
-'4401011' : 'Ariel Casanella',
-'4401020' : 'Abhiram Dawar',
-'514005' : 'Eliel Esquivel',
-'514007' : 'Andrew Fuentes',
-'44001790': 'Yahira Herrera',
-'44003688': 'Riyakumari Jain',
-'15404' : 'Cristina Morocho',
-'13249' : 'Nidhi Parekh',
-'4401121' : 'Max Quevedo',
-'302319' : 'Marianna Said',
-'4401094' : 'Sebastian Tapia'}
-
 while True:
     try:
         s_id = input('Scan student id or enter Q to quit: ')
